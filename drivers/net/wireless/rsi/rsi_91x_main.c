4085402bea150b3b3cef97b788ee65cc739bca46
Prameela Rani Garnepudi <prameela.j04cs@gmail.com>
2017-11-29 15:10:34 +0530

    rsi: roaming enhancements
    To support roaming below changes are done:
    * Station notify frame is send to firmware after sending assoc
      request. This will avoid dropping of first EAPOL frame due to
      delay in creation of station control block in firmware.
    * Data queues are unblocked after sending station notify in open
      mode, after configuring key in WEP mode, and after receiving
      EAPOL4 confirm in WPA mode.
    * Initial EAPOL frames priority is chaged to MGMT, rekey EAPOL
      frames priority changed to VO.
    * LMAC clock is changed to 80MHz by default.
    * Data frames with wrong BSSID are dropped.
    
    Signed-off-by: Prameela Rani Garnepudi <prameela.j04cs@gmail.com>

--- drivers/net/wireless/rsi/rsi_91x_main.c
+++ drivers/net/wireless/rsi/rsi_91x_main.c
@@ -24,11 +24,11 @@
 #include "rsi_coex.h"
 #include "rsi_hal.h"
 
-u32 rsi_zone_enabled = /* INFO_ZONE |
+u32 rsi_zone_enabled =  INFO_ZONE |
 			INIT_ZONE |
 			MGMT_TX_ZONE |
 			MGMT_RX_ZONE |
-			DATA_TX_ZONE |
+		/*	DATA_TX_ZONE |
 			DATA_RX_ZONE |
 			FSM_ZONE |
 			ISR_ZONE | */
